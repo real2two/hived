f1d6140b7bef196b3d47746eeb12c9e3f9f4a000
Two <njsolomonoh@gmail.com>
2021-09-10 18:33:18 -0400

    Update houseinfo.js

--- commands/houseinfo.js
+++ commands/houseinfo.js
@@ -3,7 +3,7 @@ module.exports = async (client, prefix, msg, args) => {
         `:house: **House Information**\n\n` +
         `The house's (server) name is **${msg.house.name}**. (\`${msg.house.id}\`).\n` +
         `This house was created on \`${msg.house.created.toString()}\`.\n` +
-        `There are **${msg.house.rooms.size}** rooms (channels) in this house.\n` +
-        `There are **${msg.house.members.size}** members in the message's room.`
+        `There are **${msg.house.rooms.size}** rooms (channels) in this house.\n` //+
+        //`There are **${msg.house.members.size}** members in the message's room.`
     );
 }

5096973f1d84e36f6d7de4a4dba3a58a49841e38
Two <njsolomonoh@gmail.com>
2021-09-10 18:24:19 -0400

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -2,6 +2,8 @@ module.exports = async (client, prefix, msg, args) => {
     msg.room.send(
         "I'm **Hiven**, a public Hiven bot.\n\n" +
         `:robot_face: I am in **${client.houses.size}** houses.\n` +
-        `:busts_in_silhouette: And, I'm serving **${ client.users.get(undefined) ? client.users.size - 1 : client.users.size }** users.`
+        `:busts_in_silhouette: And, I'm serving **${ client.users.get(undefined) ? client.users.size - 1 : client.users.size }** users.\n\n` +
+        `:arrow_forward: **Support house**: https://hiven.house/TzZLqn\n` +
+        `:arrow_forward: **GitHub**: https://github.com/real2two/hived`
     );
 }
